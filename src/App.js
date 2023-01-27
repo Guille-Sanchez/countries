b01e93795e502a34f84de930270cc2a5e15b55f6
Guillermo Sanchez <guillesafran@gmail.com>
2023-01-27 18:27:31 -0300

    Add countries-card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [countries, setCountries] = useState("");
+  useEffect(() => {
+    const COUNTRIES_URL =
+      "https://restcountries.com/v2/all?fields=name,flag,population,region,capital";
+    let subscribed = true;
+    if (subscribed) {
+      fetch(COUNTRIES_URL)
+        .then((response) => response.json())
+        .then((data) => {
+          setCountries(() => data);
+        });
+    }
+
+    return () => {
+      subscribed = false;
+    };
+  }, []);
+
+  if (countries.length !== 0) {
+    return (
+      <div className="App">
+        {countries.map((country, index) => {
+          //console.log(`${index} country=${country.name}`);
+          return (
+            <div key={country.name} className="country-cotainer">
+              <img
+                src={country.flag}
+                alt={country.name}
+                className="country-flags"
+              />
+              <p>{country.name}</p>
+              <p>{country.population}</p>
+              <p>{country.region}</p>
+              <p>{country.capital}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
